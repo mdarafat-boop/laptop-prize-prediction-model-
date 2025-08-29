ba4b22546367c70ca64f3515c558d280a93c9d0d
Md Yasir Arafat <sagor.ali27a72024@gmail.com>
2025-08-29 08:59:54 +0600

    rename folder name

--- laptop_prize_model/example.py
+++
@@ -1,8 +0,0 @@
-import pickle
-with open("model.pkl", "rb") as file:
-    model = pickle.load(file)
-# Demo input
-features = [[8, 512, 2.4]]
-prediction = model.predict(features)
-
-print("Predicted Laptop Price:", prediction[0])
